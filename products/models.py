ce50ea577c139a522375395760d79acfad09d845
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-06-05 10:28:33 +0100

    SHP-18__add Product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField("Product Name", max_length=250)
+    discount = models.ForeignKey("Discount", blank=True, null=True)
+
+
+class Discount(models.Model):
+    name = models.CharField("Name", max_length=250)
+    discount_percent = models.DecimalField("Percentage", max_digits=4, decimal_places=2)
+    active = models.BooleanField("Active", default=True)
+
+
+class File(models.Model):
+    name = models.CharField("Name", max_length=250, blank=True, null=True)
+    image = CloudinaryField('file')
+    active = models.BooleanField("Active", default=True)
