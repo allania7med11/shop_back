eb9a41c0b2b6241f120aff671415d3f71d1027a3
allania7med11 <allania7med11@gmail.com>
2022-06-05 16:08:14 +0100

    SHP-21__add photos from product admin page

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from products.models import Product, Discount, File, Category
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Discount)
 admin.site.register(File)
 admin.site.register(Category)
+
+
+class FileInline(admin.TabularInline):
+    model = File
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        FileInline,
+    ]
