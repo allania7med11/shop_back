56c03fd231320397658472e255e83eddba21cc2d
allania7med11 <allania7med11@gmail.com>
2022-06-05 15:24:58 +0100

    SHP-21__add product models to django admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from products.models import Product, Discount, File, Category
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Discount)
+admin.site.register(File)
+admin.site.register(Category)
