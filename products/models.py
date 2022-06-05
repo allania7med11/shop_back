c83dca6367fd6901cd22c478612d13b88f07755a
allania7med11 <allania7med11@gmail.com>
2022-06-05 13:00:30 +0100

    SHP-18__add price field

--- products/models.py
+++ products/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-# Create your models here.
+from djmoney.models.fields import MoneyField
+
 class Product(models.Model):
     name = models.CharField("Product Name", max_length=250)
+    price = MoneyField(max_digits=19, decimal_places=4, default_currency='USD')
     discount = models.ForeignKey("Discount", blank=True, null=True)
 
 
 class Discount(models.Model):
     name = models.CharField("Name", max_length=250)
-    discount_percent = models.DecimalField("Percentage", max_digits=4, decimal_places=2)
+    percent = models.DecimalField("Percentage", max_digits=5, decimal_places=2)
     active = models.BooleanField("Active", default=True)
 
 
 class File(models.Model):
     name = models.CharField("Name", max_length=250, blank=True, null=True)
-    image = CloudinaryField('file')
-    active = models.BooleanField("Active", default=True)
+    file = CloudinaryField('file')
