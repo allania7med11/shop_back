e461eb26600d20dba787a0b7e3cc0b520b27b60b
allania7med11 <allania7med11@gmail.com>
2022-06-11 13:03:19 +0100

    SHP-19__Create Api for product and category

---
+++ products/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from products.models import Category, Discount, Product
+
+class DiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        fields = ["name", "percent", "active"]
+
+class ProductSerializer(serializers.ModelSerializer):
+    discount = DiscountSerializer()
+    class Meta:
+        model = Product
+        fields = ["id", "name", "discount","category"]
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ["id", "name", "slug", "products"]
