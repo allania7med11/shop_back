949b67ec481c3ec3c8d441cf3783f1329e298272
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-06-18 11:51:03 +0100

    SHP-19__change categories url

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from products.views import CategoryViewSet, ProductViewSet
 
 router = routers.DefaultRouter()
-router.register(r"category", CategoryViewSet)
+router.register(r"categories", CategoryViewSet)
 router.register(r"products", ProductViewSet)
 
 
