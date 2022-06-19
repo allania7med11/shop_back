7d6a997f2d57e9351c0a06af96c8454d7b3ce455
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-06-19 11:28:27 +0100

    SHP-19__add price to ProductSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,19 +2,24 @@ from rest_framework import serializers
 
 from products.models import Category, Discount, Product
 
+
 class DiscountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Discount
         fields = ["name", "percent", "active"]
 
+
 class ProductSerializer(serializers.ModelSerializer):
     discount = DiscountSerializer()
+
     class Meta:
         model = Product
-        fields = ["id", "name", "discount","category"]
+        fields = ["id", "name", "price", "price_currency", "discount", "category"]
+
 
 class CategorySerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
+
     class Meta:
         model = Category
         fields = ["id", "name", "slug", "products"]
