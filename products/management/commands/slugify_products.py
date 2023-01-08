c9f7229d082754a28a0962e2ac6ced71e08552fb
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-08 13:49:09 +0100

    SHP-31__add command to add slugs to products with no slugs

---
+++ products/management/commands/slugify_products.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from products.models import Product
+from products.utils.slugify import unique_slugify
+
+
+class Command(BaseCommand):
+    help = "Command to add slugs to products with no slugs"
+    def handle(self, *args, **options):
+        products = Product.objects.all()
+        for product in products:
+            if not product.slug:
+                unique_slugify(product, product.name)
+                product.save()
+        self.stdout.write(
+            "Slugs were added successfully to products with no slugs"
+        )
