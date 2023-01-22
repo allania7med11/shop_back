b0b28de02fa6cfe3c375e721c6f54e81af629824
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-22 14:32:52 +0100

    SHP-34__return current_price after discount from api

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from unittest import case
+from django.db.models import F, Case, When, DecimalField
 from rest_framework import viewsets
 from products.filters import ProductFilter
 from products.models import Category, Product
@@ -5,21 +7,39 @@ from products.models import Category, Product
 from products.serializers import CategorySerializer, ProductSerializer
 from rest_framework import permissions
 
+
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows Products to be viewed or edited.
     """
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_class = ProductFilter
-    lookup_field = 'slug'
+    lookup_field = "slug"
     permission_classes = [permissions.AllowAny]
-    
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(
+            current_price=Case(
+                When(discount__isnull=True, then=F("price")),
+                When(
+                    discount__isnull=False,
+                    then=F("price") * (1 - F("discount__percent") / 100),
+                ),
+                output_field=DecimalField(),
+            )
+        )
+        return queryset
+
+
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows Products to be viewed or edited.
     """
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    lookup_field = 'slug'
+    lookup_field = "slug"
     permission_classes = [permissions.AllowAny]
