7347bfb20343b240e3b9073c1562c809ea191404
allania7med11 <allania7med11@gmail.com>
2023-03-31 00:03:02 +0000

    update shop_back_static setting

--- shop_back/settings.py
+++ shop_back/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "back_static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "shop_back_static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
