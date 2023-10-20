c22567068e814813ca15f61e70bdffad17471f90
allania7med11 <allania7med11@gmail.com>
2023-10-20 05:11:58 +0000

    script for develop

---
+++ core/management/commands/change_user_password_in_dev.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.hashers import make_password
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        User.all_objects.update(password=make_password("pearladmin"))
+        self.stdout.write(
+            "------- Pearl users updated to password : pearladmin---------"
+        )
