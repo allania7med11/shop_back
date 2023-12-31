684090ab9c893ad4b1ad7510a442052e6ef73cdd
allania7med11 <allania7med11@gmail.com>
2023-12-31 15:22:35 +0000

    862kpe6p4__move order to user after login

--- products/utils/orders.py
+++ products/utils/orders.py
@@ -1,25 +1,43 @@
+import uuid
 from products.models import Order, OrderItems
 
 
 def get_current_draft_order(request):
+    session_order = get_session_order(request)
     if not request.user.is_authenticated:
-        session_id = request.session.session_key
-        if not session_id:
-            # Create a new session if it doesn't exist
-            request.session.create()
-            session_id = request.session.session_key
-        order, created = Order.objects.get_or_create(
-            session_id=session_id,
+        if session_order:
+            return session_order
+        order_uuid = request.session.get("order_uuid")
+        if not order_uuid:
+            order_uuid = str(uuid.uuid4())  # Use a new UUID for the order_uuid
+            request.session["order_uuid"] = order_uuid
+        order = Order.objects.create(
+            uuid=order_uuid,
             status=Order.OrderStatus.DRAFT,
         )
-    else:
-        user = request.user
-        order, created = Order.objects.get_or_create(
-            user=user,
-            status=Order.OrderStatus.DRAFT,
-            defaults={"total_amount": 0.0},
-        )
+        return order
+    user = request.user
+    if session_order:
+        session_order.uuid = None
+        session_order.user = user
+        session_order.save()
+        return session_order
+    order, created = Order.objects.get_or_create(
+        user=user,
+        status=Order.OrderStatus.DRAFT,
+        defaults={"total_amount": 0.0},
+    )
     return order
+
+def get_session_order(request):
+    order_uuid = request.session.get("order_uuid")
+    if order_uuid:
+        order = Order.objects.filter(
+            uuid=order_uuid,
+            status=Order.OrderStatus.DRAFT,
+        ).first()
+        return order
+    return None
 
 
 def get_existing_or_new_order_item(order, product):
