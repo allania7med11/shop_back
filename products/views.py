8a5190f800e701f200a157614b6468a7c97e9199
allania7med11 <allania7med11@gmail.com>
2024-06-17 17:14:02 +0000

    create  Order Address

--- products/views.py
+++ products/views.py
@@ -1,19 +1,19 @@
-from django.db.models import F, Case, When, DecimalField
 from rest_framework import viewsets
 from products.filters import ProductFilter
 from products.mixins import CartInitiationMixin
-from products.models import Category, OrderItems, Product
+from products.models import Category, OrderAddress, OrderItems, Product
 
 from products.serializers import (
+    CartAddressSerializer,
     CartItemsSerializer,
     CategorySerializer,
     ProductSerializer,
 )
 from rest_framework import permissions, status
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-from products.utils.orders import  get_existing_or_new_order_item
+from products.utils.orders import  get_existing_or_new_order_address, get_existing_or_new_order_item
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
@@ -51,6 +51,26 @@ class CartItemsViewSet(CartInitiationMixin, viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         instance = get_existing_or_new_order_item(self.cart, request.data["product"])
+        serializer = self.get_serializer(data=request.data, instance=instance)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CardAddressViewSet(CartInitiationMixin, viewsets.ModelViewSet):
+    queryset = OrderAddress.objects.all()
+    serializer_class = CartAddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return OrderAddress.objects.filter(order=self.cart)
+
+    def create(self, request, *args, **kwargs):
+        order = self.cart 
+        if not order.user == self.request.user:
+            return Response({"detail": "Order not found for the current user."}, status=status.HTTP_400_BAD_REQUEST)
+
+        instance = get_existing_or_new_order_address(order, request.data)
         serializer = self.get_serializer(data=request.data, instance=instance)
         if serializer.is_valid():
             serializer.save()
