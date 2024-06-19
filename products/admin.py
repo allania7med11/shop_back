14a3cf3b20b6549f19d82632ac9bac90518b61b9
allania7med11 <allania7med11@gmail.com>
2024-06-19 14:42:01 +0000

    SHP-2405245__add Order to admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from products.models import Product, Discount, File, Category
+from products.models import Order, OrderAddress, OrderItems, Product, Discount, File, Category
 
 admin.site.site_header = "Shoppingify Admin"
 admin.site.site_title = "Shoppingify Admin Portal"
@@ -26,3 +26,24 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     fields = ['name']
+
+
+class OrderAddressInline(admin.StackedInline):
+    model = OrderAddress
+    can_delete = False
+    verbose_name_plural = 'Order Address'
+
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItems
+    extra = 1
+    verbose_name_plural = 'Order Items'
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'order_date', 'total_amount', 'status')
+
+    inlines = [OrderAddressInline, OrderItemsInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.exclude(status=Order.OrderStatus.DRAFT)
