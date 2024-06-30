49905e555d8b4de9d59101207904399e9bbc4533
allania7med11 <allania7med11@gmail.com>
2024-06-30 09:07:34 +0000

    SHP-2806245__fix save_stripe_info

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,7 @@
+import json
 from django.conf import settings
+from django.http import JsonResponse
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,34 +9,45 @@ from rest_framework.decorators import api_view
 from .serializers import CardInformationSerializer
 import stripe
 
+
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-@api_view(['POST'])
+@api_view(["POST"])
 def test_payment(request):
     test_payment_intent = stripe.PaymentIntent.create(
-        amount=1000, currency='pln', 
-        payment_method_types=['card'],
-        receipt_email='test@example.com')
+        amount=1000, currency="pln", 
+        payment_method_types=["card"],
+        receipt_email="test@example.com")
     return Response(status=status.HTTP_200_OK, data=test_payment_intent)
 
+
+
 def save_stripe_info(request):
-    data = request.data
-    email = data['email']
-    payment_method_id = data['payment_method_id']
-    
-    # creating customer
-    customer = stripe.Customer.create(
-        email=email,
-        payment_method=payment_method_id
-    )
-     
-    return Response(
-        status=status.HTTP_200_OK, 
-        data={
-            'message': 'Success', 
-            'data': {'customer_id': customer.id}
-        }
-    )
+    # Ensure that method is POST and handle JSON loading
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            email = data["email"]
+            payment_method_id = data["payment_method_id"]
+        except (json.JSONDecodeError, KeyError) as e:
+            return JsonResponse({'message': f'Error parsing data: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            # Create a customer in Stripe
+            customer = stripe.Customer.create(
+                email=email,
+                payment_method=payment_method_id,
+                invoice_settings={
+                    'default_payment_method': payment_method_id
+                }
+            )
+            return JsonResponse({'message': 'Success', 'customer_id': customer.id}, status=status.HTTP_200_OK)
+        except stripe.error.StripeError as e:
+            # Handle Stripe API errors
+            return JsonResponse({'message': str(e)}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+    else:
+        return JsonResponse({'message': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
 
 class PaymentAPI(APIView):
     serializer_class = CardInformationSerializer
@@ -46,8 +60,8 @@ class PaymentAPI(APIView):
             response = self.stripe_card_payment(data_dict=data_dict)
         else:
             response = {
-                'errors': serializer.errors,
-                'status': status.HTTP_400_BAD_REQUEST
+                "errors": serializer.errors,
+                "status": status.HTTP_400_BAD_REQUEST
             }
                 
         return Response(response)
@@ -57,55 +71,55 @@ class PaymentAPI(APIView):
             card_details = stripe.PaymentMethod.create(
                 type="card",
                 card={
-                    "number": data_dict['card_number'],
-                    "exp_month": data_dict['expiry_month'],
-                    "exp_year": data_dict['expiry_year'],
-                    "cvc": data_dict['cvc'],
+                    "number": data_dict["card_number"],
+                    "exp_month": data_dict["expiry_month"],
+                    "exp_year": data_dict["expiry_year"],
+                    "cvc": data_dict["cvc"],
                 },
             )
             # You can also get the amount from the database by creating a model
             payment_intent = stripe.PaymentIntent.create(
                 amount=10000,  # Amount in the smallest currency unit (e.g., 10000 for $100.00)
-                currency='inr',
+                currency="inr",
             )
             payment_intent_modified = stripe.PaymentIntent.modify(
-                payment_intent['id'],
-                payment_method=card_details['id'],
+                payment_intent["id"],
+                payment_method=card_details["id"],
             )
             try:
                 payment_confirm = stripe.PaymentIntent.confirm(
-                    payment_intent['id']
+                    payment_intent["id"]
                 )
-                payment_intent_modified = stripe.PaymentIntent.retrieve(payment_intent['id'])
+                payment_intent_modified = stripe.PaymentIntent.retrieve(payment_intent["id"])
             except Exception as e:
-                payment_intent_modified = stripe.PaymentIntent.retrieve(payment_intent['id'])
+                payment_intent_modified = stripe.PaymentIntent.retrieve(payment_intent["id"])
                 payment_confirm = {
                     "stripe_payment_error": "Failed",
-                    "code": payment_intent_modified['last_payment_error']['code'],
-                    "message": payment_intent_modified['last_payment_error']['message'],
-                    'status': "Failed"
+                    "code": payment_intent_modified["last_payment_error"]["code"],
+                    "message": payment_intent_modified["last_payment_error"]["message"],
+                    "status": "Failed"
                 }
-            if payment_intent_modified and payment_intent_modified['status'] == 'succeeded':
+            if payment_intent_modified and payment_intent_modified["status"] == "succeeded":
                 response = {
-                    'message': "Card Payment Success",
-                    'status': status.HTTP_200_OK,
+                    "message": "Card Payment Success",
+                    "status": status.HTTP_200_OK,
                     "card_details": card_details,
                     "payment_intent": payment_intent_modified,
                     "payment_confirm": payment_confirm
                 }
             else:
                 response = {
-                    'message': "Card Payment Failed",
-                    'status': status.HTTP_400_BAD_REQUEST,
+                    "message": "Card Payment Failed",
+                    "status": status.HTTP_400_BAD_REQUEST,
                     "card_details": card_details,
                     "payment_intent": payment_intent_modified,
                     "payment_confirm": payment_confirm
                 }
         except Exception as e:
             response = {
-                'error': "Your card number is incorrect",
-                'status': status.HTTP_400_BAD_REQUEST,
+                "error": "Your card number is incorrect",
+                "status": status.HTTP_400_BAD_REQUEST,
                 "payment_intent": {"id": "Null"},
-                "payment_confirm": {'status': "Failed"}
+                "payment_confirm": {"status": "Failed"}
             }
         return response
