754d270c0b46183dfcde75cb5c9ce0dea1d3f645
allania7med11 <allania7med11@gmail.com>
2024-07-13 16:07:40 +0000

    SHP-2806245__add id to CartProductSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -89,6 +89,7 @@ class CartProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = [
+            "id",
             "name",
             "files",
         ]
