f39492f03d4576109e3cf23947c89caef6a99f6c
allania7med11 <allania7med11@gmail.com>
2025-02-12 14:31:16 +0000

    add UserProfileViewSet to upload and get profile_photo

--- authentication/models.py
+++ authentication/models.py
@@ -1,1 +1,8 @@
-# Create your models here.
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    profile_photo = CloudinaryField("file", null=True, blank=True)
